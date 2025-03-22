716cba2fc0973c0fbec349772adfd01e864b822f
chadgolden1 <chad@chadgolden.com>
2025-03-22 13:07:48 -0400

    Add linting to CI

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from '@typescript-eslint/eslint-plugin'
 import tsparser from '@typescript-eslint/parser'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules', 'public', 'external'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
